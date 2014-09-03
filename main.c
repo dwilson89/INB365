b1aadea435ab0da32ab9ce31caf6380a561d9ed7
Dustin <dwilson89@users.noreply.github.com>
2014-09-03 21:55:46 +1000

    Added my student number Added in some Defines Added in some more global vars to keep track of stuff Added in some new function to random generate plane code and whether to generate a plane - should be changed as to reuse code for indicating a takeoff Added in code and psuedo code for producer thread

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 // 
 //	Authors:
 //	Chris Rhodan n6862624
-//	Dustin Wilson <add student number>
+//	Dustin Wilson n6325157
 //
 //	Due Date:
 // 	9th September 2014
@@ -14,22 +14,39 @@
 #include <time.h>
 #include <pthread.h>
 
+#define MAXIMUM_AIRPORT_CAPACITY 10
+#define AIRPLANE_CODE 6
+
+// Not sure if this compiler allows bools
+#define TRUE 1
+#define FALSE 0 
+
 // Struct to represent a single Airplane
 struct Airplane{
-	char code[6];
+	char code[AIRPLANE_CODE];
 	time_t parkTime;
 };
 
-// Array of Pointers to Airplanes to represent the Airport
-struct Airplane *airport[10];
+// Array of Pointers to Airplanes to represent the Airport - shared resource
+struct Airplane *airport[MAXIMUM_AIRPORT_CAPACITY];
 
 // Variables to contain the odds of a plane landing and departing
 int arrivalOdds = 0;
 int departureOdds = 0;
 
+// Variable to determine if run way is free or occupied; has true for free, 
+// false for occupied - shared resource
+//bool isRunwayFree = true;
+int isRunwayFree = TRUE;
+
+// Keeps track of airport capacity
+int currentAirportCapacity = 0;
+
+int keep_running = 1;
+
 // Function to allocate memory for each Airplane in the airport
 void InitialiseAirport(){
-	for(int i = 0; i < 10; i++){
+	for(int i = 0; i < MAXIMUM_AIRPORT_CAPACITY; i++){
 		airport[i] = malloc(sizeof(struct Airplane));
 	}
 }
@@ -43,15 +60,103 @@ void *UserControls(){
 	pthread_exit(NULL);
 }
 
+char *CreateAirplaneCode(){
+
+	int ranNum = 0;
+	int counter = 0;
+
+	//ASCII for 'A' is 65, '0' is 48
+	char code[AIRPLANE_CODE];
+	for(int i = 0; i < AIRPLANE_CODE; i++){
+		airport[i] = malloc(sizeof(char));
+	}
+
+	while (counter < AIRPLANE_CODE){
+
+		// random generate 2 Alpha chars A-Z
+		if(counter < 2) {
+
+			ranNum = 26 * (rand() / (RAND_MAX + 1.0));
+			ranNum = ranNum + 65;
+			code[counter] = (char)ranNum;
+
+		} else {// random generate 4 numerical chars 0-9
+
+			ranNum = 10 * (rand() / (RAND_MAX + 1.0));
+			ranNum = ranNum + 48;
+			code[counter] = (char)ranNum;
+
+		}
+		counter++;
+	}
+	return code;
+}
+
+int IsPlaneGenerated(){
+
+	int ranNum = 0;
+	int isGenerated = 0;
+
+	// Calculate a value between 0 and 100
+	ranNum = 100 * (rand() / (RAND_MAX + 1.0));
+
+	// If value is in arrivalOdds range 0 - arrivalOdds (it has that chance to be in that range)
+	if(ranNum < arrivalOdds) {
+
+		isGenerated = 1;
+	}
+
+	return isGenerated;
+}
+
+// Producer
 void *AirportArrival(){
 
 	printf("DEBUG: Airport Arrival Started\n");
 
 	// TODO: Add Code
 
+	while(keep_running){
+
+		// Possibly a loop that keeps it here until room - blocking
+		while(currentAirportCapacity == MAXIMUM_AIRPORT_CAPACITY){
+			printf("DEBUG: The airport is full");
+			sleep(1);
+		}
+
+		sleep(500);
+		
+		// check the Runway - might need to swap this around
+		if(isRunwayFree){
+			
+			isRunwayFree = FALSE;
+			// Is a new plane to be generated
+			if(IsPlaneGenerated()){
+
+			// Create a new plane	
+
+			// if landing printf("DEBUG: Plane %s is landing", newPlane.code);
+
+			// Sleep for 2 seconds? or just assign current time? Time for landing
+
+			// Need to randomly generate a empty landing bay number
+
+			// assign landing bay
+
+			// if landed printf("DEBUG: Plane %s parked in landing bay %d", assignedBay);
+				
+			}
+		// Free up runway
+		isRunwayFree = TRUE;
+		}
+
+	}
+	
 	pthread_exit(NULL);
 }
 
+
+// Consumer
 void *AirportDepart(){
 
 	printf("DEBUG: Airport Depart Started\n");
